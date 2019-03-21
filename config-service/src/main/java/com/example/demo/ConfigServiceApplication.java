6cca674e509235295964154c80b4deec8a32a8c3
Asus <Asus@DESKTOP-7FSGV49>
2019-03-20 22:28:33 -0500

    add git repository call: http://localhost:8888/reservation/master

--- config-service/src/main/java/com/example/demo/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/demo/ConfigServiceApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
